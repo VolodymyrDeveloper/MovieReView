49583f529c3b0177c2db89f3bc751ec0766da73c
Roman Matsuiev <romanmatsuiev@gmail.com>
2022-02-04 20:15:54 +0200

    init-commit

---
+++ src/main/java/com/cursor/lobo/moviereview/MovieReViewApplication.java
@@ -0,0 +1,13 @@
+package com.cursor.lobo.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReViewApplication.class, args);
+    }
+
+}
